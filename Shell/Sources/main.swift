37886cc5d2698be94b5ad1eb49ac106b24fcc95b
Ayşıl Simge Karacan <github@aysilsimge.dev>
2025-09-08 15:27:14 +0300

    implement type command

--- Shell/Sources/main.swift
+++ Shell/Sources/main.swift
@@ -1,24 +1,38 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+enum ShellCommand: String {
+    case echo, type, exit
+}
+
 while true {
     print("$ ", terminator: "")
 
     if let input = readLine() {
         let inputArray = input.split(separator: " ")
-        let command = inputArray.first
+        let firstInput: String? = inputArray.first.map { String($0) }
         let arguments = inputArray.dropFirst()
 
-        guard input != "exit 0", let command else {
+        guard input != "exit 0",
+            let firstInput,
+            let command =
+                ShellCommand(rawValue: firstInput)
+        else {
             break
         }
 
-        if command == "echo" {
+        switch command {
+        case .echo:
             print(arguments.joined(separator: " "))
-        } else {
-
+        case .type:
+            let argumentString = arguments.joined()
+            if ShellCommand(rawValue: argumentString) != nil {
+                print("\(argumentString) is a shell builtin")
+            } else {
+                print("\(argumentString): not found")
+            }
+        default:
             print("\(input): command not found")
         }
-
     }
 }
